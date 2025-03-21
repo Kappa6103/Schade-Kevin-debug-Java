ed72a322828bcb2d0b0e757bd40cc41a05b9bdaa
Kappa6103 <schadekevin@protonmail.com>
2025-03-21 17:00:56 +0100

    everything is working logic is nice and tidy going to refactor next

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,14 +1,13 @@
 package com.hemebiotech.analytics;
 
-import java.io.FileWriter;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
 
-	public static <listOfSymptoms> void main(String[] args) throws Exception { //error in the main declaration syntax
+	public static void main(String[] args) { //error in the main declaration syntax
 
 		//create the hashmap in which the diseases + num of occurrence will be written
 		Map<String, Integer> symptomsHashMap = new HashMap<String, Integer>();
@@ -16,10 +15,10 @@ public class AnalyticsCounter {
 		//instantiating ReadSymptomDataFromFile, this load an object with the file path to be read.
 		ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile("symptoms.txt");
 
-		//using the .GetSymptoms() methode of the weadSymptomDataFromFile object to load in a string the content read from the file
+		//using the .GetSymptoms() methode of the readSymptomDataFromFile object to load in a string the content read from the file
 		List<String> listOfSymptoms = readSymptomDataFromFile.GetSymptoms();
 
-		//iterating through the Strings present in listOfSymptoms
+		//iterating through the Strings present in listOfSymptoms and building the unsorted hashMap of names of symptoms and the number of their occurrences.
 		for(String item : listOfSymptoms) {
 			if (symptomsHashMap.containsKey(item)) { //if the name of a symptom is already present
 				symptomsHashMap.put(item, symptomsHashMap.get(item) + 1); //increase its count to n + 1
@@ -28,16 +27,22 @@ public class AnalyticsCounter {
 			}
 		}
 
+		//creating a TreeMap to sort the hashMap containing the key/value name/occurrence in alphabetical order
+		TreeMap<String, Integer> sortedHashMap = new TreeMap<String, Integer>(symptomsHashMap);
+
+
 
 //		for (Map.Entry<String, Integer> entry : symptomsHashMap.entrySet()) {
 //			System.out.println(entry.getKey() + " " + entry.getValue());
 //		}
 
+		//
+
 		//instantiating WriteSymptomDateToFile, this load an object with the file path to be written
-		WriteSymptomDataToFile writeSymptomDataToFile = new WriteSymptomDataToFile("resultooOut.txt");
+		WriteSymptomDataToFile writeSymptomDataToFile = new WriteSymptomDataToFile("result.out");
 
 		//using the .writeSymptoms method of the writeSymptomDataToFile object to print formatted string of the curated hasMap onto a file
-		writeSymptomDataToFile.writeSymptoms(symptomsHashMap);
+		writeSymptomDataToFile.writeSymptoms(sortedHashMap);
 
 
 
