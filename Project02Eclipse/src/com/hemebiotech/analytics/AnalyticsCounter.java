ee6c82c5cfad82382f86cd0020887aedafcb65f7
Kappa6103 <schadekevin@protonmail.com>
2025-03-21 09:46:55 +0100

    removing unnecessary comment and cleaning up the logique.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -8,33 +8,38 @@ public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
+	private static int diseaseNotAccountedFor = 0; //it's to know if we have read all diseas from the file
+
+	public static void main(String[] args) throws Exception { //error in the main declaration syntax
+
+		//reading the text file
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+
 		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				headacheCount++;
 			}
-			else if (line.equals("rush")) {
+			else if (line.equals("rash")) {
 				rashCount++;
 			}
 			else if (line.contains("pupils")) {
 				pupilCount++;
+			} else {
+				diseaseNotAccountedFor++;
 			}
-
-			line = reader.readLine();	// get another symptom
+			line = reader.readLine();
 		}
+		//closing file's reading method
+		reader.close();
+		System.out.println("Headache: " + headacheCount);
+		System.out.println("Rash: " + rashCount);
+		System.out.println("Pupils: " + pupilCount);
+		System.out.println("Not accounted for: " + diseaseNotAccountedFor);
 		
 		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
+		FileWriter writer = new FileWriter ("resultOut.txt");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
